9a24d9861c3a33edbc6b62ba7b22315ebf890f06
William <il.wchan97@gmail.com>
2021-04-27 18:54:52 -0500

    Completed active rides page

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const MongoClient = require("mongodb").MongoClient;
-const url =
-  "mongodb+srv://BrandonDotson:Kelapo290@cluster0.6ikkz.mongodb.net/transportation_health_app?retryWrites=true&w=majority";
+const url = "mongodb+srv://BrandonDotson:Kelapo290@cluster0.6ikkz.mongodb.net/transportation_health_app?retryWrites=true&w=majority";
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
+//Creating variables for current date, and tomorrow date
+
+/**
+ * Creating a date one day ahead of current date for the purpose of determining 
+ * whether a trip is onTrack or not. This string will maintain leadin zeros for days and months.
+ */
+
+var currentDate = new Date();
+var tomorrowDateString, todayDateString;
+currentDate.setDate(currentDate.getDate() + 1);
+tomorrowDateString = (currentDate.getFullYear() + "-" + ("0" + currentDate.getMonth() + 1)).slice(-2) +
+  "-" + ("0" + currentDate.getDate()).slice(-2);
+
+/**
+ * creating a variable 'todayDateString' in which current date is stored in a way that 
+ * the leading zeros of days and months are mainted.
+ */
+
+currentDate.setDate(currentDate.getDate());
+todayDateString = (currentDate.getFullYear() + "-" + ("0" + currentDate.getMonth() + 1)).slice(-2) +
+  "-" + ("0" + currentDate.getDate()).slice(-2);
+//connect mongodb
 MongoClient.connect(url, { useUnifiedTopology: true })
   .then((client) => {
     console.log("Connected to Database");
@@ -38,11 +59,41 @@ MongoClient.connect(url, { useUnifiedTopology: true })
         })
         .catch((error) => console.error(error));
     });
-
+    /** Dashboard page wherein the count of trips are displayed separately according to theier status
+     *In addition to that it displays the trips that are on track or not. The criteria for being on track
+      is an active status
+    */
     app.get("/", function (req, res) {
-      res.render("dashboard");
+      var scheculedTripArr = [];
+      var activeRiedArr = [];
+      var previousTripArr = [];
+      custinfoCollection.find({}).toArray(function (err, customerinfoD) {
+        if (err) {
+          console.log("Cannot get dashboard page. " + err);
+        } else {
+          custinfoCollection.find({ status: 'active' }).count().then((ar_count) => {
+            activeRiedArr.push(ar_count);
+            custinfoCollection.find({ status: 'completed' }).count().then((pt_count) => {
+              previousTripArr.push(pt_count);
+              custinfoCollection.find({ status: 'upcoming' }).count().then((st_count) => {
+                scheculedTripArr.push(st_count);
+                res.render("dashboard", {
+                  title: "Dashboard",
+                  scheduledTrips: st_count,
+                  customerinfo: customerinfoD,
+                  activeRides: ar_count,
+                  previousTrips: pt_count
+                });
+              });
+            });
+          });
+        }
+      });
     });
-
+    /** Scheduled Appointments page wherein all the appointments regardless of their status are
+     *meant to be displayed as default. Options for filtering has been emplaced but not implemented
+     * Here a status filter (upcoming) has explicitly been made.
+     */
     app.get("/scheduled", function (req, res) {
       var arr = [];
       custinfoCollection.find({}).toArray(function (err, customerinfoR) {
@@ -51,26 +102,28 @@ MongoClient.connect(url, { useUnifiedTopology: true })
         } else {
           arr = customerinfoR;
           var result_from_mongodb = [];
-          custinfoCollection.countDocuments().then((count) => {
+          custinfoCollection.find({ status: 'upcoming' }).count().then((count) => {
             result_from_mongodb.push(count);
             res.render("scheduled", {
               result: result_from_mongodb,
               customerinfo: customerinfoR,
-              length: count,
+              length: customerinfoR.length,
+              len: count
             });
           });
-          //res.redirect('/');
         }
-        // console.log(
-        //   "this is a print form the screen 2" + JSON.stringify(customerinfoR)
-        // );
       });
-      // get count
     });
-
-    app.get("/active-ride", function (req, res) {
-      res.render("active-rides");
+    // Active Rides page where only rides whose status is 'active' is displayed
+    app.get("/active-rides", function (req, res) {
+      custinfoCollection.find({ status: 'active' }).toArray(function (err, customerinfoG) {
+        if (err) { console.log('Cannot get active-rides page: ' + err); } else {
+          res.render('active-rides', {
+            title: 'Active Rides',
+            customerinfo: customerinfoG
+          })
+        }
+      })
     });
-
   })
   .catch((error) => console.error(error));
